f915860dcc90a40cd1be83a6dcd61ef0952de35c
eshakar <102459513+eshakar@users.noreply.github.com>
2025-03-11 21:27:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+	event.preventDefault();
+
+	const firstName = document.getElementById("firstName").value;
+	const lastName = document.getElementById("lastName").value;
+	const phoneNumber = document.getElementById("phoneNumber").value;
+	const emailId = document.getElementById("emailId").value;
+
+	const alertMessage = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`;
+
+	alert(alertMessage);
+})
